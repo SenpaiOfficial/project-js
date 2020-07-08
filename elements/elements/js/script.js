08926bbd87de34689c80d5b9c83827c6f7fbe288
Aydos <example@gmail.com>
2020-07-08 14:56:15 +0600

    feat: add new code lesson

---
+++ elements/elements/js/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const box = document.getElementById('box');
+
+console.log(box);
+
+const btns = document.getElementsByTagName('button');
+
+console.log(btns[0]);
+
+const circles = document.getElementsByClassName('circle');
+
+console.log(circles);
+
+const hearts = document.querySelectorAll('.heart');
+
+hearts.forEach(item => {
+ console.log(item);
+});
+
+// С помощью forEach перебирает элементы/классы через item колбэк
+
+const oneHeart = document.querySelector('.heart');
+
+console.log(oneHeart);
